879711b7fa8db4448dd68bfbf612fd5f35e8322b
OlehLeo <ozaruma@cloverdynamics.com>
2023-03-01 22:12:10 +0200

    added body parser

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import templateController from './template.js';
 
 const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json({ limit: '50mb' }));
 
 router.use('/template', templateController);
 
