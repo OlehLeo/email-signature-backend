e904fae0d37ef9745f138fe61357c367ef8393c9
OlehLeo <ozaruma@cloverdynamics.com>
2023-03-01 21:12:22 +0200

    added test endpoint

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ import router from './router.js';
 
 const app = express();
 
+app.get('/api/test', (req, res) => {
+  res.end('Welcome from test endpoint');
+});
+
 app.use(cors());
 app.use(router);
 
