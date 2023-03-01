320c13ed8812b2eb7fcf4dd95b5c5fdbff19b7bd
OlehLeo <ozaruma@cloverdynamics.com>
2023-03-01 21:23:56 +0200

    restructured test endpoint

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import templateController from './controllers/template.js';
+import templateController from '../controllers/template.js';
 
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json({ limit: '50mb' }));
 
+router.get('/', async function (req, res) {
+  res.send(`<h1>Hello from homepage!</h1>`);
+});
 router.use('/template', templateController);
 
-export default router;
+module.exports = router;
